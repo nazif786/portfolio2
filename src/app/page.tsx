944f5b970147996d9dd146a703f2a7281e688777
Nazifullah <nazif.it@gmail.com>
2024-02-18 12:48:47 +0530

    configure shadCN

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1> portfolio </h1>
+      <Button> click here </Button>
     </div>
   );
 }

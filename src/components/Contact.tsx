39fd0ab2ef4e7332713cbea7b0ce4782eab2a163
Nazifullah <nazif.it@gmail.com>
2024-03-18 23:08:39 +0530

    build contact me form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "./ui/textarea";
+import { Button } from "./ui/button";
 
 const Contact = () => {
   return (
-    <section className="flex flex-row items-center justify-center w-[100%] bg-ring">
-      <div className="">Contact</div>
-      <div className="">Contact</div>
+    <section className="flex flex-col md:flex-row justify-between w-[100%] my-16 p-2 md:p-3 min-h-[250px] md:min-h-[700px] rounded-md">
+      <div className="bg-[url('/contactIcon.svg')] hidden md:block bg-no-repeat bg-contain md:bg-cover md:w-[49%] h-[600px] rounded-s-md"></div>
+      <div className="md:w-[49%] text-slate-50 md:my-10 md:mx-5">
+        <div className="text-center text-xl font-semibold mb-3">
+          <h1>Send Me an Email:</h1>
+        </div>
+        <form className="flex flex-col space-y-6">
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Input type="email" id="email" placeholder="Email" />
+          </div>
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Input type="text" id="subject" placeholder="subject" />
+          </div>
+          <div className="grid w-full gap-1.5">
+            <Textarea
+              className="h-[250px]"
+              placeholder="Type your message here."
+              id="message"
+            />
+          </div>
+          <div className="flex  w-full items-end justify-end">
+            <Button type="submit" variant="outline">
+              Send Email
+            </Button>
+          </div>
+        </form>
+      </div>
     </section>
   );
 };

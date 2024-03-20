932f37313e159ed930af86014308857bf22e98a7
Nazifullah <nazif.it@gmail.com>
2024-03-20 11:27:07 +0530

    added icons

---
+++ src/components/ui/icons/skillsIcons/SkillIconsVercelDark.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import type { SVGProps } from 'react';
+
+export function SkillIconsVercelDark(props: SVGProps<SVGSVGElement>) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256" {...props}><g fill="none"><rect width={256} height={256} fill="#242938" rx={60}></rect><path fill="#fff" d="m128 34l95 164.853H33z"></path></g></svg>);
+}

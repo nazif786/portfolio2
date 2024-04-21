621eaf032c67940176af0a3856291537475c45f8
Nazifullah <nazif.it@gmail.com>
2024-04-22 04:15:52 +0530

    update photo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <header>
+          <header className="">
             <Nav />
           </header>
 
---
+++ src/app/working/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <section className="h-dvh bg-ring">
+      <div className="h-full bg-background rounded-ee-full w-11/12">
+        {" "}
+        this is new page
+      </div>
+    </section>
+  );
+};
+
+export default page;
